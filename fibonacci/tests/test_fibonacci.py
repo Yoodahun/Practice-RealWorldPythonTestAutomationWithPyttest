c54698d90817a218c82529a609127cc3b7704a39
Yoodahun <dahun4032@gmail.com>
2023-02-24 23:14:56 +0900

    ADD dynamic.py

--- fibonacci/tests/test_fibonacci.py
+++ fibonacci/tests/test_fibonacci.py
@@ -3,6 +3,7 @@ from typing import Callable, List, Tuple
 import pytest
 
 from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
+from fibonacci.dynamic import fibonacci_dynamic, fibonacci_dynamic_v2
 from fibonacci.naive import fibonacci_naive
 from fixture import time_tracker
 
@@ -29,7 +30,7 @@ def test_naive(n: int, expected: int):
     assert fibonacci_naive(n) == expected
 
 
-@pytest.mark.parametrize("fib_func ", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
+@pytest.mark.parametrize("fib_func ", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached, fibonacci_dynamic, fibonacci_dynamic_v2])
 @pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 def test_fibonacci(time_tracker, fib_func: Callable[[int], int], n: int, expected: int):
     res = fib_func(n)
