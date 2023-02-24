0cba41fed7aac073ea1bde10cfd1c21578157d20
Yoodahun <dahun4032@gmail.com>
2023-02-24 21:38:25 +0900

    ADD fibonacci function and cached function

--- fibonacci/tests/test_fibonacci.py
+++ fibonacci/tests/test_fibonacci.py
@@ -1,8 +1,35 @@
+from typing import Callable, List, Tuple
+
 import pytest
 
+from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
 from fibonacci.naive import fibonacci_naive
 
 
+# Decorator = Callable
+# def my_parameterize(identifiers: str, values: List[Tuple[int, int]]) -> Decorator:
+#     def my_parameterized_decorator(function: Callable) -> Callable:
+#         def run_func_parameterized():
+#             list_of_kwargs_for_function = get_list_of_kwargs_for_function(
+#                 identifiers=identifiers,
+#                 values=values
+#             )
+#             for kwargs_for_function in list_of_kwargs_for_function:
+#                 print(
+#                     f"calling function {function.__name__} with {kwargs_for_function}"
+#                 )
+#                 function(**kwargs_for_function)
+#
+#     return my_parameterized_decorator
+
 @pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
-def test_naive(n:int, expected:int):
+# @my_parameterize(identifiers="n, expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_naive(n: int, expected: int):
     assert fibonacci_naive(n) == expected
+
+
+@pytest.mark.parametrize("fib_func ", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
+@pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int):
+    res = fib_func(n)
+    assert res == expected
