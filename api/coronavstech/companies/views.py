9eaf87bacbd33d04c875689ebf5edadeb7e16c5c
Yoodahun <dahun4032@gmail.com>
2023-02-24 15:12:13 +0900

    ADD commenting

--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -1,5 +1,8 @@
+from django.core.mail import send_mail
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from api.coronavstech.companies.models import Company
 from api.coronavstech.companies.serializers import CompanySerializer
@@ -10,3 +13,12 @@ class CompanyViewSet(ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all().order_by("-last_update")
     pagination_class = PageNumberPagination
+
+# @api_view(http_method_names=["POST"])
+# def send_company_email(request):
+#
+#     send_mail(subject="My cool subject", message="My cool message",
+#               from_email="test@test.com",
+#               recipient_list=["dahun4032@gmail.com"]
+#               )
+#     return Response({"status":"success", "info":"email sent successfully."}, status=200)
